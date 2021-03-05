26080dd87631a051bb3aa786b26cd4452945dee8
michelwene <michelwene@hotmail.com>
2021-03-04 22:41:41 -0300

    Exercicios

---
+++ Exercicio2/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2
+{
+    class Produto
+    {
+        private int codigo;
+        private string nome;
+        private int estoque;
+        private Double valor;
+
+
+        public Produto()
+        {
+
+        }
+        public Produto(int codigo, string nome, int estoque, double valor)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.estoque = estoque;
+            this.valor = valor;
+            Console.WriteLine("Estocagem de produtos: ");
+        }
+        public void adicionarProdutos(int estoque)
+        {
+            this.estoque = estoque;
+            Console.WriteLine("Entrada de produtos: " + estoque);
+
+        }
+    
+        public void retirarProduto(int estoque)
+        {
+            this.estoque -= estoque;
+            Console.WriteLine("Retirada de produtos: -" + estoque + "\n");
+        }
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Código: "+ codigo);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Estoque:" + estoque);
+            Console.WriteLine("Valor: " + valor+"\n");
+
+        }
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto(1, "Celular", 0, 1999.99);
+            produto.ImprimirDados();
+            produto.adicionarProdutos(20);
+            produto.retirarProduto(5);
+            Produto produto1 = new Produto (1,"Celular", 15, 1999.99);
+            produto.ImprimirDados();
+        }
+    }
+}
